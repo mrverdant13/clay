5ac433f1453f879d8fd772bcee811136f7475f2e
Karlo Verde <karlo.verde.1996@gmail.com>
2026-06-15 23:29:38 -0500

    refactor: simplify

--- packages/clay_core/lib/src/features/config/parse_config_map.dart
+++ packages/clay_core/lib/src/features/config/parse_config_map.dart
@@ -1,10 +1,8 @@
 import 'package:clay_core/src/entities/clay_config.dart';
 import 'package:clay_core/src/features/config/matches_ignore_pattern.dart';
-import 'package:clay_core/src/features/config/validate_environment.dart';
 
 /// Parses a config map into a [ClayConfig] and validates ignore patterns.
 ClayConfig parseConfigMap(Map<String, dynamic> map) {
-  validateEnvironmentMap(map);
   final config = ClayConfig.fromMap(map);
   validateIgnorePatterns(config.ignore);
   return config;
